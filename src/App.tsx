1422801a80daa97a085088525a89323a30a4384e
sk-phan <sukiphan97@gmail.com>
2023-11-22 21:50:29 +0200

    install toaster for alert

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 import "./globals.css"
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster"
+
 const App = () => {
     return (
         <main className="flex h-screen">
@@ -20,6 +22,8 @@ const App = () => {
                     <Route index element={<Home />}/>
                  </Route>
             </Routes>
+
+            <Toaster />
         </main>
     )
 }
